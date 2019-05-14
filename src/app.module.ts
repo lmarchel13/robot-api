2de6c46cbd991cb13c6806be327a1268d8255594
Lucas Marchel <lucasmarchel13@gmail.com>
2019-05-14 00:58:58 -0300

    feat: create /cotacoes and /ordens routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { mongoConnection } from '../config/database.module';
 import { OrdensModule } from './modules/ordens/ordens.module';
 import { CotacaoModule } from './modules/cotacao/cotacao.module';
 import { MONGO_URL } from 'config/keys';
+import { CotacaoService } from './modules/cotacao/cotacao.service';
+import { OrdensService } from './modules/ordens/ordens.service';
 
 @Module({
   imports: [mongoConnection(MONGO_URL), CotacaoModule, OrdensModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CotacaoService, OrdensService],
 })
 export class AppModule {}
