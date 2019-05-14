9a63e6b56a168dfe9b5312a726f270755dc0fe8d
Lucas Marchel <lucasmarchel13@gmail.com>
2019-05-14 00:58:43 -0300

    feat: create a mvc application

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const PORT = 3000;
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', 'views/partials'));
 
   await app.listen(PORT);
   Logger.log(`Server running at http://localhost:${PORT}`, 'ROBOT API');
